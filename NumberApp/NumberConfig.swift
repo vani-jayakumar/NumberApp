07759c85fd43af45562327432d2e3a8bb53ef469
Vani Lakshmi <kjvanilakshmi@gmail.com>
2024-08-29 20:07:54 +0530

    Initial Commit

---
+++ NumberApp/NumberConfig.swift
@@ -0,0 +1,13 @@
+//
+//  NumberConfig.swift
+//  NumberApp
+//
+//  Created by Vani on 8/29/24.
+//
+
+import Foundation
+
+struct NumberDataModel {
+    let number: Int
+    let counts: [[[Int]]]
+}
